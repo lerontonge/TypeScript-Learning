92fd1dd9d3d2cd93c9f48f49031321f8b780133a
lerontonge <lerontonge@gmail.com>
2022-06-19 06:54:08 -0400

    typescriptlearning

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,19 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+let sales = 12345;
+let course = 'Ts';
+let is_published = true;
+let numbers = [1, 2, 3];
+
+
+let plusTen = numbers.forEach(value => value + 10);
+console.log(plusTen);
+
+
+
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2021);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-let age: number = 20;    
+/*let age: number = 20;    
 if (age < 50)
  age += 10;
+ console.log(age); */
+
+  let sales = 12345;
+  let course = 'Ts';
+  let is_published = true;
+
+  let numbers = [1,2,3]
+  let plusTen = numbers.forEach(value => value + 10 )
+
+  console.log (plusTen)
+
+
+  function calculateTax(income:number, taxYear=2022): number {
+    if (taxYear < 2022)
+        return income * 1.2
+    return income * 1.3
+  }
+
+  calculateTax(10000, 2021)
